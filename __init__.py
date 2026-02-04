5cd48f75b00e699761b3f5f3e4048c9684a3d57f
gabriellebrieanna <gabriellebrieanna@gmail.com>
2026-02-04 14:39:02 -0600

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,14 @@
+# Here is the file.
+
+import numpy
+import pyscf
+
+#from cymods.density import density_comp
+from pymods.hylleraas_e import hylleraas_e
+from pymods.hylleraas_en import hylleraas_en
+from pymods.hylleraas_n import hylleraas_n
+from pymods.mp2_dens import mp2_density
+from pymods.rmsd import rmsd
+from pymods.t_amps_e_only import t_amps_e_only
+from pymods.t_amps_en_only import t_amps_en_only
+from pymods.t_amps_n_only import t_amps_n_only
